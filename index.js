8eb3be57872052d748bc33bef830f07a0e95e606
manhhungpc <phamhung2002@gmail.com>
2021-04-19 22:54:42 +0700

    19-04-2021 update

---
+++ index.js
@@ -0,0 +1,28 @@
+//just for update content
+$("document").ready(function () {
+  //Tuesday - Thu 3
+  $("#Tue1").text("Anh");
+  $("#Tue2").text("Anh");
+  $("#Tue3").text("Hóa");
+  $("#Tue4").text("Hóa");
+  //Wednesday - Thu 4
+  $("#Wed1").text("Sinh");
+  $("#Wed2").text("Sinh");
+  $("#Wed3").text("Lý");
+  $("#Wed4").text("Lý");
+  //Thursday - Thu 5
+  $("#Thur1").text("Anh");
+  $("#Thur1").text("Anh");
+  $("#Thur1").text("Toán");
+  $("#Thur1").text("Toán");
+  //Friday - Thu 6
+  $("#Fri1").text("Sinh");
+  $("#Fri2").text("Sinh");
+  $("#Fri3").text("Hóa");
+  $("#Fri4").text("Hóa");
+  //Saturday - Thu 7
+  $("#Sat1").text("Văn");
+  $("#Sat2").text("Văn");
+  $("#Sat3").text("Hóa");
+  $("#Sat4").text("Hóa");
+});
